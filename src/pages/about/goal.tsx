393660890cf057da27e18b7034c1bb9bd1211c63
RikThePixel <63408969+Rikthepixel@users.noreply.github.com>
2022-08-02 00:38:02 +0200

    rename pages

---
+++ src/pages/about/goal.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Goal = () => {
+    return (
+        <div>Work in progress</div>
+    );
+};
+
+export default Goal;
---
+++ src/pages/about/privacy.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Privacy = () => {
+  return (
+    <div>Work in progress</div>
+  )
+}
+
+export default Privacy
