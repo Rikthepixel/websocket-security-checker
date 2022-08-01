9fbbef74feab0f8286d3778818d6a229367d4054
RikThePixel <63408969+Rikthepixel@users.noreply.github.com>
2022-08-01 22:48:07 +0200

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/provider';
 import type { AppProps } from "next/app";
+import Head from 'next/head';
 
 const App = ({ Component, pageProps }: AppProps) => (
-    <ChakraProvider>
-        <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+        <Head>
+            <title>WS Checker</title>
+            <meta property="og:title" content="WS Checker" key="title" />
+        </Head>
+        <ChakraProvider>
+            <Component {...pageProps} />
+        </ChakraProvider>
+    </>
 );
 
 export default App;
